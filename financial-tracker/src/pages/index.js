1911057d05c7e07a55ece49320d7e270a63100b9
Angus Lau <anguslau91@gmail.com>
2024-05-20 18:25:12 -0700

    ui change

--- financial-tracker/src/pages/index.js
+++ financial-tracker/src/pages/index.js
@@ -6,9 +6,11 @@ import ExpenseModal from '@/Components/ExpenseModal';
 
 const App = () => {
     const [data, setData] = useState({});
+    const headerText = 'BMO Expense Tracker';
 
     return (
         <div id='react-box'>
+            <h1>BMO Expense Tracker</h1>
             <Expense setData={setData} data={data} />
             <div class='buttons'>
             <Buttons/>
